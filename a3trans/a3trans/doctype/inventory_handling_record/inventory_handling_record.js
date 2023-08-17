6a5b6ea95bf58c14ed59b24c318c4e6ffad61e27
Nijina Acube <nijina.a@acube.co>
2023-08-17 19:23:08 +0530

    Doctypes

---
+++ a3trans/a3trans/doctype/inventory_handling_record/inventory_handling_record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Nijina A and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Inventory Handling Record', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ a3trans/a3trans/doctype/task_assignment/test_task_assignment.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Nijina A and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestTaskAssignment(unittest.TestCase):
+	pass
