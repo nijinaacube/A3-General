85fc015025e168dba2a761e3d42f1870594ba3aa
Nijina <nijina.a@acube.co>
2023-10-06 19:09:16 +0530

    warehouse address

--- a3trans/a3trans/events/warehouse.py
+++ a3trans/a3trans/events/warehouse.py
@@ -15,58 +15,58 @@ import frappe
     
     
 
-def after_insert(doc,methods):
-    address=frappe.new_doc("Address")
-    address.address_type="Warehouse"
-    address.address_title="Office"
-    if doc.address_line_1:
-        address.address_line1 = doc.address_line_1
-    else:
-        address.address_line_1 ="NIL"
-    if doc.address_line_2:
-        address.address_line2 = doc.address_line_2
-    else:
-        address.address_line_2 = "NIL"
-    if doc.city:
-        address.city = doc.city
-    else:
-        address.city="NIL"
-    if doc.latitude:
-        address.latitude = doc.latitude
-    if doc.longitude:
-        address.longitude = doc.longitude
+# def after_insert(doc,methods):
+#     address=frappe.new_doc("Address")
+#     address.address_type="Warehouse"
+#     address.address_title="Office"
+#     if doc.address_line_1:
+#         address.address_line1 = doc.address_line_1
+#     else:
+#         address.address_line_1 ="NIL"
+#     if doc.address_line_2:
+#         address.address_line2 = doc.address_line_2
+#     else:
+#         address.address_line_2 = "NIL"
+#     if doc.city:
+#         address.city = doc.city
+#     else:
+#         address.city="NIL"
+#     if doc.latitude:
+#         address.latitude = doc.latitude
+#     if doc.longitude:
+#         address.longitude = doc.longitude
   
-    if doc.email_id:
-        address.email_id = doc.email_id
-    if doc.phone_no:
-        address.phone=doc.phone_no
-    address.append("links", {
-							"link_doctype": "Warehouse",
-							"link_name": doc.name
-						})
-    address.save()
+#     if doc.email_id:
+#         address.email_id = doc.email_id
+#     if doc.phone_no:
+#         address.phone=doc.phone_no
+#     address.append("links", {
+# 							"link_doctype": "Warehouse",
+# 							"link_name": doc.name
+# 						})
+#     address.save()
 
-    if not frappe.db.exists("Contact", {"first_name":doc.warehouse_name, "mobile_no":doc.phone_no}):
-        contact = frappe.new_doc("Contact")
-        print("hiii")
-        contact.first_name = doc.warehouse_name
-        if doc.email_id:
-            contact.append("email_ids", {
-            "email_id": doc.email_id,
-            "is_primary": 1,
-        })
-        contact.append("phone_nos", {
-            "phone": doc.phone_no,
-            "is_primary_phone": 1,
-            "is_primary_mobile_no": 1,
-        })
-        contact.append("links", {
-            "link_doctype": "Warehouse",
-            "link_name": doc.name,
-            "link_title": doc.name,
-        })
+#     if not frappe.db.exists("Contact", {"first_name":doc.warehouse_name, "mobile_no":doc.phone_no}):
+#         contact = frappe.new_doc("Contact")
+#         print("hiii")
+#         contact.first_name = doc.warehouse_name
+#         if doc.email_id:
+#             contact.append("email_ids", {
+#             "email_id": doc.email_id,
+#             "is_primary": 1,
+#         })
+#         contact.append("phone_nos", {
+#             "phone": doc.phone_no,
+#             "is_primary_phone": 1,
+#             "is_primary_mobile_no": 1,
+#         })
+#         contact.append("links", {
+#             "link_doctype": "Warehouse",
+#             "link_name": doc.name,
+#             "link_title": doc.name,
+#         })
         
-        contact.insert()
+#         contact.insert()
 
 
 @frappe.whitelist()
