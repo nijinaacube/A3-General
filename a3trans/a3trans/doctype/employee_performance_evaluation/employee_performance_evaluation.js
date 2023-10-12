c57f4e81af5de77187277d49b0e5852ff7ae7d17
user <nijina.a@acube.co>
2023-10-12 11:46:33 +0530

    performance evaluation

--- a3trans/a3trans/doctype/employee_performance_evaluation/employee_performance_evaluation.js
+++ a3trans/a3trans/doctype/employee_performance_evaluation/employee_performance_evaluation.js
@@ -3,9 +3,32 @@
 
 frappe.ui.form.on('Employee Performance Evaluation', {
 	onload: function(frm) {
+		console.log("hiii")
 		if  (frm.is_new()){
             frm.set_value('date', frappe.datetime.get_today());
 			frm.set_value('rated_by',frappe.session.user)
+			if (frm.doc.order_id){
+				frappe.call({
+					method: "a3trans.a3trans.doctype.employee_performance_evaluation.employee_performance_evaluation.get_employee",
+					args: {
+						"doc": frm.doc.order_id
+					},
+					callback: (r) => {
+						console.log(r.message);
+		
+							
+							cur_frm.set_value("employee", r.message["employee"]);
+							cur_frm.refresh_field("employee");
+							cur_frm.set_value("trip_id", r.message["va_id"]);
+							cur_frm.refresh_field("trip_id");
+
+	
+							
+	
+						
+					}
+				});
+			}
           }
 
 
--- a3trans/a3trans/doctype/employee_performance_evaluation/employee_performance_evaluation.py
+++ a3trans/a3trans/doctype/employee_performance_evaluation/employee_performance_evaluation.py
@@ -1,8 +1,24 @@
 # Copyright (c) 2023, Nijina A and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class EmployeePerformanceEvaluation(Document):
 	pass
+@frappe.whitelist()
+def get_employee(doc):
+	print(doc)
+	opp=frappe.get_doc("Opportunity",doc)
+	va=frappe.get_doc("Vehicle Assignment",{"order":doc})
+	data={}
+	if opp.driver:
+		staff=frappe.get_doc("Staff Member",opp.driver)
+		data["employee"]=staff.employee
+		data["va_id"]=va.name
+		return data
+
+	
+         
+        
+ 
