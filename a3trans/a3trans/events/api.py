7f8e45db5d850d22604bf50970fa67a2655b0347
Ramit Panangat <101082621+ramitacube@users.noreply.github.com>
2023-11-22 17:29:24 +0530

    Update api.py with lead_name

--- a3trans/a3trans/events/api.py
+++ a3trans/a3trans/events/api.py
@@ -49,7 +49,7 @@ def create_new_lead(data: dict, booking_type: BookingType, hasPrev: bool):
         lead.mobile_number = data["mobile_number"]
     lead.booking_channel = "Mobile App"
     lead.booking_type = booking_type.name
-    lead.lead_owner = data["email_id"]
+    # lead.lead_owner = data["email_id"]
     lead.remarks = data["remarks"]
     if booking_type.inventory_required == 1:
         lead.warehouse = data["warehouse"]
